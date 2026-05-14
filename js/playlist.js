98eddaa4b8874ae49cb746e772a44d63c315f87c
Timofei Sila <42304025+sila-timofei@users.noreply.github.com>
2026-05-14 15:31:11 +0300

    Update playlist.js

--- js/playlist.js
+++ js/playlist.js
@@ -27,7 +27,7 @@ const playlists = {
         image: "img/playlist-groom.jpg",
 
         iframe: `
-            <iframe frameborder="0" allow="clipboard-write" style="border:none;width:100%;height:314px;" width="100%" height="314" src="https://music.yandex.ru/iframe/playlist/posta-vip/1054">Слушайте <a href='https://music.yandex.ru/users/posta-vip/playlists/1054'>Groom exit</a> — <a href='https://music.yandex.ru/users/posta-vip'>SILA</a> на Яндекс Музыке</iframe>
+            <iframe frameborder="0" allow="clipboard-write" style="border:none;width:100%;height:314px;" src="https://music.yandex.ru/iframe/playlist/posta-vip/1054">Слушайте <a href="https://music.yandex.ru/playlists/08f84f3a-d358-50b7-99e3-36048aed4bf6?utm_source=web&utm_medium=copy_link">Groom exit</a> — <a href="https://music.yandex.ru/users/posta-vip">SILA</a> на Яндекс Музыке</iframe>
         `
     },
 
