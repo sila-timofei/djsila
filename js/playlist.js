0ece509190e407b85395f2a7ceec3025273de49c
Timofei Sila <42304025+sila-timofei@users.noreply.github.com>
2026-05-14 16:23:11 +0300

    Update playlist.js

--- js/playlist.js
+++ js/playlist.js
@@ -117,7 +117,7 @@ const playlists = {
         image: "img/playlist-hen-paty.jpg",
 
         iframe: `
-            <iframe frameborder="0" allow="clipboard-write" style="border:none;width:100%;height:300px;" width="100%" height="300" src="https://music.yandex.ru/iframe/playlist/posta-vip/1058">Слушайте <a href="https://music.yandex.ru/playlists/ef8127e0-9711-8ce3-b5e0-cade03f59486?utm_source=web&utm_medium=copy_link">Get ready</a> — <a href="https://music.yandex.ru/users/posta-vip">SILA</a> на Яндекс Музыке</iframe>
+            <iframe frameborder="0" allow="clipboard-write" style="border:none;width:100%;height:312px;" src="https://music.yandex.ru/iframe/playlist/posta-vip/1060">Слушайте <a href='https://music.yandex.ru/users/posta-vip/playlists/1060'>Bachelorette party</a> — <a href='https://music.yandex.ru/users/posta-vip'>SILA</a> на Яндекс Музыке</iframe>
         `
     },
 
@@ -132,7 +132,7 @@ const playlists = {
         image: "img/playlist-weekend-together.jpg",
 
         iframe: `
-            <iframe frameborder="0" allow="clipboard-write" style="border:none;width:100%;height:300px;" width="100%" height="300" src="https://music.yandex.ru/iframe/playlist/posta-vip/1058">Слушайте <a href="https://music.yandex.ru/playlists/ef8127e0-9711-8ce3-b5e0-cade03f59486?utm_source=web&utm_medium=copy_link">Get ready</a> — <a href="https://music.yandex.ru/users/posta-vip">SILA</a> на Яндекс Музыке</iframe>
+            <iframe frameborder="0" allow="clipboard-write" style="border:none;width:100%;height:312px;" src="https://music.yandex.ru/iframe/playlist/posta-vip/1059">Слушайте <a href='https://music.yandex.ru/users/posta-vip/playlists/1059'>Stay home</a> — <a href='https://music.yandex.ru/users/posta-vip'>SILA</a> на Яндекс Музыке</iframe>
         `
     },
 
@@ -146,7 +146,7 @@ const playlists = {
         image: "img/playlist-exit-bride.jpg",
 
         iframe: `
-            <iframe frameborder="0" allow="clipboard-write" style="border:none;width:100%;height:300px;" width="100%" height="300" src="https://music.yandex.ru/iframe/playlist/posta-vip/1058">Слушайте <a href="https://music.yandex.ru/playlists/ef8127e0-9711-8ce3-b5e0-cade03f59486?utm_source=web&utm_medium=copy_link">Get ready</a> — <a href="https://music.yandex.ru/users/posta-vip">SILA</a> на Яндекс Музыке</iframe>
+            <iframe frameborder="0" allow="clipboard-write" style="border:none;width:100%;height:312px;" src="https://music.yandex.ru/iframe/playlist/posta-vip/1055">Слушайте <a href='https://music.yandex.ru/users/posta-vip/playlists/1055'>Bride exit</a> — <a href='https://music.yandex.ru/users/posta-vip'>SILA</a> на Яндекс Музыке</iframe>
         `
     },
 
